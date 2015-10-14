7cba1c5cc724f0692884a8b1ce4548ed6ff0eea5
user_name <email_id>
2015-10-14 15:39:55 +0300

    first commit

---
+++ register.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<title>User Login</title>
+<link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+<body>
+<form name="frmUser" method="post" action="">
+<div class="message"><?php if($message!="") { echo $message; } ?></div>
+<table border="0" cellpadding="10" cellspacing="1" width="500" align="center" bgcolor="red">
+<tr class="tableheader">
+<td align="center" colspan="2"><strong><b><font size="35">Details of Person</font></b></strong></td>
+</tr>
+<tr class="tablerow">
+<td align="right">Username</td>
+<td><input type="text" name="user_name"></td>
+</tr>
+<tr class="tablerow">
+<td align="right">Email</td>
+<td><input type="text" name="user_name"></td>
+</tr>
+<tr class="tablerow">
+<td align="right">Password</td>
+<td><input type="password" name="password"></td>
+</tr>
+<tr class="tableheader">
+<td align="center" colspan="2"><input type="submit" name="submit" value="Send"></td>
+</tr>
+<tr class="tableheader">
+<td align="center" colspan="2"><a href="index.php">Sign In</a></td>
+</tr>
+</table>
+</form>
+</body></html>
